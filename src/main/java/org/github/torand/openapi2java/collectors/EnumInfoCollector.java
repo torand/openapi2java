073ca3b8b0fc14dda079d0dbeb5f5ad3e63a6fe4
Tore Eide Andersen <tore.eide.andersen@kantega.no>
2024-03-25 23:06:16 +0100

    refactor: extract enum collector

---
+++ src/main/java/org/github/torand/openapi2java/collectors/EnumInfoCollector.java
@@ -0,0 +1,47 @@
+package org.github.torand.openapi2java.collectors;
+
+import io.swagger.v3.oas.models.media.Schema;
+import org.github.torand.openapi2java.Options;
+import org.github.torand.openapi2java.model.EnumInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static java.lang.Boolean.TRUE;
+import static org.github.torand.openapi2java.utils.StringHelper.normalizeDescription;
+
+public class EnumInfoCollector {
+    private final Options opts;
+
+    public EnumInfoCollector(Options opts) {
+        this.opts = opts;
+    }
+
+    public EnumInfo getEnumInfo(String name, Schema<?> schema) {
+        EnumInfo enumInfo = new EnumInfo();
+
+        enumInfo.annotations.add(getSchemaAnnotation(name, schema, enumInfo.imports));
+
+        if (TRUE.equals(schema.getDeprecated())) {
+            enumInfo.annotations.add("@Deprecated");
+        }
+
+        enumInfo.constants.addAll((List<String>)schema.getEnum());
+
+        return enumInfo;
+    }
+
+    private String getSchemaAnnotation(String name, Schema<?> pojo, Set<String> imports) {
+        String description = pojo.getDescription();
+
+        imports.add("org.eclipse.microprofile.openapi.annotations.media.Schema");
+        List<String> schemaParams = new ArrayList<>();
+        schemaParams.add("name = \"%s\"".formatted(name));
+        schemaParams.add("description=\"%s\"".formatted(normalizeDescription(description)));
+        if (TRUE.equals(pojo.getDeprecated())) {
+            schemaParams.add("deprecated = true");
+        }
+        return "@Schema(%s)".formatted(String.join(", ", schemaParams));
+    }
+}
--- src/main/java/org/github/torand/openapi2java/writers/EnumWriter.java
+++ src/main/java/org/github/torand/openapi2java/writers/EnumWriter.java
@@ -2,26 +2,37 @@ package org.github.torand.openapi2java.writers;
 
 import io.swagger.v3.oas.models.media.Schema;
 import org.github.torand.openapi2java.Options;
+import org.github.torand.openapi2java.collectors.EnumInfoCollector;
+import org.github.torand.openapi2java.model.EnumInfo;
 
 import java.io.Writer;
 
-import static org.github.torand.openapi2java.utils.StringHelper.normalizeDescription;
+import static org.github.torand.openapi2java.utils.CollectionHelper.nonEmpty;
 
 public class EnumWriter extends BaseWriter {
+    private final EnumInfoCollector enumInfoCollector;
 
     public EnumWriter(Writer writer, Options opts) {
         super(writer, opts);
+        this.enumInfoCollector = new EnumInfoCollector(opts);
     }
 
-    public void write(String name, Schema schema) {
+    public void write(String name, Schema<?> schema) {
+        EnumInfo enumInfo = enumInfoCollector.getEnumInfo(name, schema);
+
         writeLine("package %s;", opts.getModelPackage());
         writeNewLine();
-        writeLine("import org.eclipse.microprofile.openapi.annotations.media.Schema;");
-        writeNewLine();
-        writeLine("@Schema(name = \"%s\", description=\"%s\")".formatted(name, normalizeDescription(schema.getDescription())));
+
+        if (nonEmpty(enumInfo.imports)) {
+            enumInfo.imports.forEach(ti -> writeLine("import %s;".formatted(ti)));
+            writeNewLine();
+        }
+
+        enumInfo.annotations.forEach(a -> writeLine(a));
+
         writeLine("public enum %s {".formatted(name));
-        write("    ");
-        writeLine(String.join(", ", schema.getEnum()));
+        writeIndent(1);
+        writeLine(String.join(", ", enumInfo.constants));
         writeLine("}");
     }
 }
